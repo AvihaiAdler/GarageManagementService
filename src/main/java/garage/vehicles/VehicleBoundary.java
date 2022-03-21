21698617bb89796cc478bb32060cadef4a91cd67
Avihai Adler <avihai.adler@gmail.com>
2022-03-21 16:29:13 +0200

    minor changes

--- src/main/java/garage/vehicles/VehicleBoundary.java
+++ src/main/java/garage/vehicles/VehicleBoundary.java
@@ -1,17 +1,11 @@
 package garage.vehicles;
 
-import java.util.List;
-
-import garage.vehicles.util.EnergySource;
-import garage.vehicles.util.VehicleType;
-import garage.vehicles.util.Wheel;
-
-public record VehicleBoundary (VehicleType vehicleType,
-        EnergySource energySource,
-        List<Wheel> wheels,
+public record VehicleBoundary (String vehicleType,
         String modelName,
         String licenseNumber,
         Integer availableEnergyPercentage,
-        Integer maxTirePressure) { }
+        Integer maxTirePressure) { 
+  
+}
 
   
