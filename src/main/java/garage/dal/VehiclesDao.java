43ed239f6744f30a065ad083d2d1cfc1b927c908
Avihai Adler <avihai.adler@gmail.com>
2022-03-21 18:13:11 +0200

    added more methods queries

--- src/main/java/garage/dal/VehiclesDao.java
+++ src/main/java/garage/dal/VehiclesDao.java
@@ -1,9 +1,20 @@
 package garage.dal;
 
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import garage.data.VehicleEntity;
 
-import garage.data.VehiclesEntity;
 
-public interface VehiclesDao extends MongoRepository<VehiclesEntity, String> {
-
+public interface VehiclesDao extends MongoRepository<VehicleEntity, String> {
+  
+  public Optional<VehicleEntity> findByLicenseNumber(@Param("licenseNumber") String licenseNumber);
+  
+  public Page<VehicleEntity> findAllByVehicleType(@Param("vehicleType") String vehicleType, Pageable page);
+  
+  public Page<VehicleEntity> findAllByEnergySource(@Param("energySource") String energySource, Pageable page);
+  
+  public Page<VehicleEntity> findAllByModelName(@Param("modelName") String modelName, Pageable page);
 }
