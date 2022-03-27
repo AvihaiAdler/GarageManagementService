83574b04d1885e4fb0279dce22b21c4f98250620
Avihai Adler <avihai.adler@gmail.com>
2022-03-27 19:48:57 +0300

    with @query

--- src/main/java/garage/data/dal/EnergySourceDao.java
+++ src/main/java/garage/data/dal/EnergySourceDao.java
@@ -1,10 +1,13 @@
 package garage.data.dal;
 
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import garage.data.EnergySourceEntity;
 
 public interface EnergySourceDao extends PagingAndSortingRepository<EnergySourceEntity, String> {
+  @Query("select e from EnergySourceEntity e where e.energyType = :energyType")
   Optional<EnergySourceEntity> findByEnergyType(@Param("energyType") String energyType);
 }
--- src/main/java/garage/data/dal/VehicleTypeDao.java
+++ src/main/java/garage/data/dal/VehicleTypeDao.java
@@ -2,11 +2,13 @@ package garage.data.dal;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import garage.data.VehicleTypeEntity;
 
 public interface VehicleTypeDao extends PagingAndSortingRepository<VehicleTypeEntity, String> {
+  @Query("select v from VehicleTypeEntity v where v.vehicleType = :vehicleType")
   Optional<VehicleTypeEntity> findByVehicleType(@Param("vehicleType") String vehicleType);
 }
